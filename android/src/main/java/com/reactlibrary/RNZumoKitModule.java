70d68466b7920547523c1d275b77d2219c072057
Stephen Radford <steve228uk@gmail.com>
2019-03-20 09:02:42 +0000

    Use Java 1.8

--- android/src/main/java/com/reactlibrary/RNZumoKitModule.java
+++ android/src/main/java/com/reactlibrary/RNZumoKitModule.java
@@ -7,10 +7,10 @@ import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.WritableMap;
-import java.util.Date;
-import java.util.TimeZone;
-import java.text.SimpleDateFormat;
 
+import com.blockstar.zumokit.ZumoCore;
+import com.blockstar.zumokit.Store;
+import com.blockstar.zumokit.State;
 import com.blockstar.zumokit.Currency;
 import com.blockstar.zumokit.Keystore;
 
@@ -18,20 +18,31 @@ public class RNZumoKitModule extends ReactContextBaseJavaModule {
 
   private final ReactApplicationContext reactContext;
 
+  private ZumoCore zumoCore;
+  private Store zumoStore;
+  private State zumoState;
+
   public RNZumoKitModule(ReactApplicationContext reactContext) {
     super(reactContext);
-    
+
     this.reactContext = reactContext;
-
-    Keystore newKeystore = new Keystore("c6800133-79b2-44d8-b5e8-168a04652886", Currency.ETH,
-      "7357589f8e367c2c31f51242fb77b350a11830f3", "keystore_json");
-
   }
 
   @ReactMethod
   public void initialize() {
 
-    // THis method is callable from the JS side.
+    // Abstracting this out into a method so it's not called before we need it.
+    // Ultimately this will have URL and keys like the current JS implementation.
+
+    String dbPath = this.reactContext
+      .getFilesDir()
+      .getAbsolutePath();
+
+    HttpImpl httpImpl = new AndroidHttp();
+
+    this.zumoCore = ZumoCore.init(dbPath, httpImpl);
+    this.zumoStore = this.zumoCore.store();
+    this.zumoState = zumoStore.getState();
 
   }
 
