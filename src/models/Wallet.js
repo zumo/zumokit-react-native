e2b2095afac0bdd85119154f28dbb36aafcd184b
Stephen Radford <steve228uk@gmail.com>
2019-03-25 15:18:41 +0000

    Start populating the wallet with methods

--- src/models/Wallet.js
+++ src/models/Wallet.js
@@ -1,3 +1,29 @@
+/**
+ * Represents a wallet on the blockchain/Zumo network.
+ *
+ * @export
+ * @class Wallet
+ */
 export default class Wallet {
     
+    /**
+     * The address of the wallet on the blockchain.
+     *
+     * @memberof Wallet
+     */
+    address;
+
+    constructor(json) {
+        if(!json) throw 'JSON required to construct a Wallet.';
+    }
+
+    /**
+     * Loads the balance for the wallet in ETH.
+     *
+     * @memberof Wallet
+     */
+    async getBalance() {
+        
+    }
+
 }
