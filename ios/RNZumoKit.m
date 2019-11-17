adfa13c708029d3e5ee0e385a30f1f8ba7465223
Stephen Radford <steve228uk@gmail.com>
2019-11-17 16:55:52 +0000

    Fiat price on iOS

--- ios/RNZumoKit.m
+++ ios/RNZumoKit.m
@@ -319,7 +319,7 @@ RCT_EXPORT_METHOD(clear:(RCTPromiseResolveBlock)resolve rejector:(RCTPromiseReje
     if([transaction gasLimit]) dict[@"gasLimit"] = [transaction gasLimit];
     if([transaction submittedAt]) dict[@"submittedAt"] = [transaction submittedAt];
     if([transaction confirmedAt]) dict[@"confirmedAt"] = [transaction confirmedAt];
-    if([transaction fiatPrice]) dict[@"fiatPrice"] = [transaction fiatPrice];
+    if([transaction fiatCost]) dict[@"fiatValue"] = [transaction fiatCost];
     
     return dict;
 }
